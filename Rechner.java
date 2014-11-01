d7a64fa804d3764c97097dcde95c8fb9a64608d0
MSevener <maxireg@web.de>
2014-11-01 10:16:57 +0100

    RechnerKlasseHinzugefuegt
    Eine neue Datei hinzugefuegt.

---
+++ Rechner.java
@@ -0,0 +1,9 @@
+public class Rechner{
+	public static int addiere(int wert1, int wert2){
+		return wert1 + wert2;
+	}
+	
+	public static int subtrahiere(int wert1, int wert2){
+		return wert1 - wert2;
+	}
+}
