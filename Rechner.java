8196a39ae67c4cf77a1670a88b804d725a290936
MSevener <maxireg@web.de>
2014-11-01 10:44:27 +0100

    MultiplizierenImplementiert
    Funktionalitaet hintugefuegt

--- Rechner.java
+++ Rechner.java
@@ -6,4 +6,9 @@ public class Rechner{
 	public static int subtrahiere(int wert1, int wert2){
 		return wert1 - wert2;
 	}
+	
+	public static int multipliziere(int wert1, int wert2){
+		int ergebnis = wert1*wert2;
+		return ergebnis;
+	}
 }
